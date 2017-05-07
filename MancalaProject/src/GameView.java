3e7f00f32653d9b076af349625b4facbf2f7de16
Anique <aniquedavla@gmail.com>
2017-05-06 20:48:09 -0700

    view methods after paint , and model s to m

--- MancalaProject/src/GameView.java
+++ MancalaProject/src/GameView.java
@@ -48,22 +48,71 @@ public class GameView extends JComponent implements ChangeListener {
 
        
 	}
+	public void paintComponent(Graphics g) {
 
+		g.drawImage(boardFormat.backgroundImg(), 0, 0, this);
+
+		Graphics2D g2 = (Graphics2D) g;
+
+		for(Shape s: shapes)
+			g2.draw(s);
+
+		for(PitShape p: pits) {
+			p.fill(g2);
+		}
+
+		//player information
+		String turnInfo = "";
+		if (gameModel.getGameState() == GameModel.STATE.INCOMPLETE)
+		{
+			if (gameModel.getCurrentPlayer() == GameModel.Player.ONE) {
+				turnInfo = "Player One's Turn";
+			}
+			else {
+				turnInfo = "Player Two's Turn";
+			}
+		}
+		else if (gameModel.getGameState() == GameModel.STATE.COMPLETE)
+		{
+			turnInfo = "Final Score: " + "Player One's " + gameModel.playerScoreCard(GameModel.Player.ONE) + " vs " + "Player 2 " + gameModel.playerScoreCard(GameModel.Player.TWO);
+		}
+		g.setColor(Color.BLACK);//Set g color to black for font
+
+		g2.drawString(turnInfo, (getWidth()/2)-80,getHeight()/2); //print turn information
+	}
+
+	public void setVisibility(boolean visibility) {
+
+		this.setVisible(visibility);
+	}
+	public void setBoardFormat(BoardFormat formatType) {
+
+		boardFormat = formatType;
+		updateGame();
+	}
 	
 	@Override
 	public void stateChanged(ChangeEvent e) {
+
 		updateGame();
 
-		//stones from model
+		//marbles from model
 		marbles = gameModel.getAllPits();
 
 		int sizeOfPit = pits.size();
-		//update the pits with the stones from the model
+		//update the pits with the marbles from the model
 		for(int i = 0; i < sizeOfPit; i++) {
 			pits.get(i).setMarbles(marbles[i]);
 		}
 		repaint();
 		
 	}
+
+	public void start() {
+
+		setVisibility(true);
+		gameModel.setState("INCOMPLETE");
+	}
+
 
 }
