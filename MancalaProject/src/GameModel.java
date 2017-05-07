dc86911baff191238c1041ec0a7d22e213ed5cea
Anique <aniquedavla@gmail.com>
2017-05-06 19:31:02 -0700

    model last stone properties and others

--- MancalaProject/src/GameModel.java
+++ MancalaProject/src/GameModel.java
@@ -77,6 +77,11 @@ public class GameModel {
         return Player.TWO;
     }
 
+    /**
+     * isMancala checks if the current pit is a Mancala
+     * @param pit
+     * @return
+     */
     private boolean isMancala(int pit) {
         return (pit == mancalaP1 || pit == mancalaP2);
     }
@@ -167,6 +172,154 @@ public class GameModel {
             gameState = STATE.COMPLETE;
         }
     }
+    public void save() {
+        pastPits = pits.clone();
+    }
+
+    /**
+     *changePlayer changes the turn
+     */
+    private void changePlayer() {
+
+        if (currentPlayer == Player.ONE) {
+            currentPlayer = Player.TWO;
+        }
+        else {
+            currentPlayer = Player.ONE;
+        }
+    }
+
+    /**
+     *moveLastStonesTo Mancala moves the remining stone to mancala respectively
+     */
+    private void moveLastStonesToMancala() {
+        // adds up stones from all pits and places them in corresponding mancalas
+        for (int i = 0; i < totalPits; ++i)
+
+            //check if pit is not a player's mancala
+            if (!isMancala(i)) {
+                if (playerOfPit(i) == Player.ONE) {
+
+                    pits[mancalaP1] += pits[i];
+                    pits[i] = 0;
+                }
+                else {
+
+                    pits[mancalaP2] += pits[i];
+                    pits[i] = 0;
+                }
+            }
+    }
+
+    /**
+     * checkLastStoneProperties performs properties of if the last stone falls on
+     * the current players' mancal or empty pit
+     * @param currentPit
+     */
+    public void checkLastStoneProperties(int currentPit) {
+        // where is the last pit where a piece was placed
+
+        // own mancala
+        if (playerOfPit(currentPit) == currentPlayer && isMancala(currentPit))
+        {
+            //free turn
+            lastStoneInMancala = true;
+        }
+        // empty pit on your side
+        else if (playerOfPit(currentPit) == currentPlayer && pits[currentPit] == 1 && pits[oppositePit(currentPit)] > 0)
+        {
+            // collects stolen stones + stone
+            int stolenStones = pits[currentPit] + pits[oppositePit(currentPit)];
+            pits[currentPit] = pits[oppositePit(currentPit)] = 0;
+            if (currentPlayer == Player.ONE)
+            {
+                pits[mancalaP1] += stolenStones;
+            }
+            else {
+                pits[mancalaP2] += stolenStones;
+            }
+            // change turns
+            lastStoneInMancala = false;
+            changePlayer();
+        }
+        else {
+            // change turns
+            lastStoneInMancala = false;
+            changePlayer();
+        }
+    }
+
+    /**
+     *arePitsEmpty checks if current player's pits are empty
+     * @return true is yes they are empty, false otherwise
+     */
+    private boolean arePitsEmpty() {
+        int player1Pits = 0;
+        int player2Pits = 0;
+        for (int i = 0; i < totalPits; ++i)
+
+            //make sure pit is not a Mancala
+            if (!isMancala(i)) {
+                if (playerOfPit(i) == Player.ONE) //compares players
+                {
+                    player1Pits += pits[i];
+                }
+                else
+                {
+                    player2Pits += pits[i];
+                }
+            }
+
+        return (player1Pits == 0 || player2Pits == 0);
+    }
+
+    /**
+     *
+     * @param pitIndex
+     */
+    public void move(int pitIndex) {
+        // save state
+        this.save();
+
+        undoable = true;
+        //reset player's undos
+        if(currentPlayer.equals(Player.ONE))
+            undosP2 = 0;
+        else
+            undosP1 = 0;
+
+        int stones = pits[pitIndex];
+        pits[pitIndex] = 0;
+
+        int currentPit = pitIndex;
+        while (stones > 0)
+        {
+            currentPit++;
+            // currentPit returns to first pit after going around
+            if (currentPit >= totalPits) currentPit = 0;
+
+            // Skip if not currentPlayer's mancala
+            if (isMancala(currentPit) && playerOfPit(currentPit) != currentPlayer) {
+                continue;
+            }
+
+            // Place stone in pit
+            pits[currentPit]= pits[currentPit] + 1;
+            stones--;
+        }
+
+        //game properties of the last stone
+        checkLastStoneProperties(currentPit);
+
+        //is pits are empty, meaning not done.
+        if (arePitsEmpty())
+        {
+            moveLastStonesToMancala();
+            gameState = STATE.COMPLETE;
+        }
+        // Notify all Listeners
+        this.notifyListeners();
+    }
 
     /**
      * undoMove undo's the last move if possible.
@@ -238,12 +391,6 @@ public class GameModel {
             return pits[mancalaP2];
         }
         return 0;
-    }
-
-
-
-    public void save() {
-        pastPits = pits.clone();
     }
 
 
