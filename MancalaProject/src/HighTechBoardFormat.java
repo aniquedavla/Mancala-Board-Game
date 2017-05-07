3dc8aa24d8695627633f967c372e28da04ef52f6
Brian Lai <brianlai@Brians-MacBook-Air.local>
2017-05-06 21:19:26 -0700

    main

---
+++ MancalaProject/src/HighTechBoardFormat.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.net.URL;
+
+public class HighTechBoardFormat implements BoardFormat {
+
+	/**
+	 * Returns the shape of the pit relative to this format
+	 * @return the shape of the pit relative to this format
+	 */
+	public Shape formatPitShape(PitShape ps) {
+		return new RoundRectangle2D.Double(ps.getX(), ps.getY(), ps.getWidth(), ps.getHeight(), 15, 15);
+	}
+
+	/**
+	 * Returns the background image for this format
+	 * @return the image for this format
+	 */
+	public Image backgroundImg() {
+		URL imgURL = this.getClass().getResource("/board2.jpg");
+		Image img = Toolkit.getDefaultToolkit().getImage(imgURL);
+        return img;
+	}
+
+}
